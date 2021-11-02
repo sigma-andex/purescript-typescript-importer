3c8f6715464e5c03985c307cbd2a4d280cd82a31
sigma-andex <sigma.andex@pm.me>
2021-11-02 13:02:49 +0000

    Fixed missing Parser.js. Update gitignore

---
+++ src/Typescript/Parser.js
@@ -0,0 +1,31 @@
+"use strict"
+
+const ts = require('typescript')
+
+exports.createProgram = (fileNames) => () => ts.createProgram(fileNames, { allowJs: true })
+
+exports.createSourceFile = (fileName) => (sourceText) => () => ts.createSourceFile(fileName, sourceText, ts.ScriptTarget.ESNext, false, ts.ScriptKind.JS)
+
+exports.getSourceFile = (program) => (fileName) => () =>
+    program.getSourceFile(fileName)
+
+exports.getSourceFiles = (program) => program.getSourceFiles()
+
+exports.getSourceFileName = (sourceFile) => sourceFile.getName()
+
+exports.getSourceFileChildren = (sourceFile) => sourceFile.getChildren()
+
+exports.getChildren = (node) => node.getChildren()
+
+exports.isTypeAliasDeclarationImpl = (node) =>
+    ts.isTypeAliasDeclaration(node) ? node : null
+
+exports.isTypeLiteralNodeImpl = (node) =>
+    ts.isTypeLiteralNode(node) ? node : null
+
+exports.isPropertySignatureImpl = (node) =>
+    ts.isPropertySignature(node) ? node : null
+
+
+exports.numberKeyword = ts.SyntaxKind.NumberKeyword
+exports.stringKeyword = ts.SyntaxKind.StringKeyword
