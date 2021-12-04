5a027ef947956b78c3f220cd172cde681bc54648
sigma-andex <sigma.andex@pm.me>
2021-12-04 20:06:38 +0000

    Add cli parser
    - uses optparse to parse command line arguments
    - currently not connected to code generation yet
    - solves #2

--- src/Main.purs
+++ src/Main.purs
@@ -2,15 +2,39 @@ module Main where
 
 import Prelude
 
-import Data.Array (intercalate)
-import Data.Tuple (fst, snd)
 import Effect (Effect)
 import Effect.Console (log)
-import GenCode (genCode)
+import Options.Applicative (Parser, argument, command, execParser, fullDesc, header, help, helper, hsubparser, info, long, metavar, progDesc, short, str, strOption, value, (<**>))
+
+data Opts = Import
+  { nodeModule :: String
+  , outputDir :: String
+  }
+
+sample :: Parser Opts
+sample = hsubparser
+  ( command "import" (info progInfo (progDesc "Import a typescript NODE_MODULE as purescript ffi"))
+  )
+  where
+  progInfo = ado
+    nodeModule <- argument str (metavar "NODE_MODULE")
+    outputDir <- strOption
+      ( long "output-dir"
+          <> short 'o'
+          <> metavar "DIR"
+          <> value "generated-src/"
+          <> help "Write output to the directory DIR. Defaults to generated-src/"
+      )
+    in Import { nodeModule, outputDir }
 
 main :: Effect Unit
-main = do
-  log "---NOT YET IMPLEMENTED---"
-  --code <- genCode [ "person/person.ts" ]
-  --log $ intercalate "\n" $ code <#> fst
-  --log $ intercalate "\n" $ code <#> snd
+main = run =<< execParser opts
+  where
+  opts = info (sample <**> helper)
+    ( fullDesc
+        <> progDesc "Runs the given command"
+        <> header "purescript <≡> typescript importer"
+    )
+
+run :: Opts -> Effect Unit
+run (Import h) = log $ show h
