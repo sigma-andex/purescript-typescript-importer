2a24916b53e6d49a59c067f8daac78d8e8fa29e9
sigma-andex <sigma.andex@pm.me>
2022-01-11 18:36:27 +0100

    Undo person testfile with namespace

--- testfiles/original/person/person.ts
+++ testfiles/original/person/person.ts
@@ -1,29 +1,25 @@
-namespace person {
+type Person = {
+    name: string,
+    age: number
+}
 
-    type Person = {
-        name: string,
-        age: number
-    }
+export function getAge(person: Person): number { return person.age }
 
-    export function getAge(person: Person): number { return person.age }
+export function createPerson(name: string, age: number): Person {
+    return { name, age }
+}
 
-    export function createPerson(name: string, age: number): Person {
-        return { name, age }
-    }
+export const johnDoe: Person = { name: "John Doe", age: 30 }
 
-    export const johnDoe: Person = { name: "John Doe", age: 30 }
+export function createPersonOrDefault(name?: string, age?: number): Person {
+    return { name: name ? name : "John", age: age ? age : 30 }
+}
 
-    export function createPersonOrDefault(name?: string, age?: number): Person {
-        return { name: name ? name : "John", age: age ? age : 30 }
-    }
+export function createPersonWithName(name: string, age?: number): Person {
+    return { name: name, age: age ? age : 30 }
+}
 
-    export function createPersonWithName(name: string, age?: number): Person {
-        return { name: name, age: age ? age : 30 }
-    }
-
-    type NullablePerson = {
-        name?: string,
-        age?: number
-    }
-
+type NullablePerson = {
+    name?: string,
+    age?: number
 }
